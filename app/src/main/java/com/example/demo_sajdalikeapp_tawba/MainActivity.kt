dd24ef149a963b53f8fc29a07bcb267899a3839a
yntymakdev <kubanychevyntymak21@gmail.com>
2025-05-14 15:18:09 +0600

    as

---
+++ app/src/main/java/com/example/demo_sajdalikeapp_tawba/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.demo_sajdalikeapp_tawba
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.demo_sajdalikeapp_tawba.ui.theme.DemosajdalikeapptawbaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DemosajdalikeapptawbaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DemosajdalikeapptawbaTheme {
+        Greeting("Android")
+    }
+}
